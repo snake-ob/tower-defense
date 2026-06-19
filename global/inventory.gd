15748f9c760ee265fb17596ecf6974e91332309f
snakeob <jakebfraser@gmail.com>
2026-06-19 09:26:37 -0400

    Began work on inventory system

---
+++ global/inventory.gd
@@ -0,0 +1,9 @@
+extends Node
+
+var max_bombs: int = 1
+var max_caltropes: int = 1
+var max_landmines: int = 1
+
+var current_bombs: int = 1
+var current_caltropes: int = 1
+var current_landmines: int = 1
